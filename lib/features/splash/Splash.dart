c9b260cfd3ea36b7d92e4bf6aed8d6e1bedacdc0
Jindongleee <jindong1019@gmail.com>
2025-05-26 14:59:37 +0900

    :sparkles:splash

---
+++ lib/features/splash/Splash.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Splash());
+}
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.orange, // 주황색 배경
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '댕댕일기', // 제목
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white, // 텍스트 색상
+                ),
+              ),
+              SizedBox(height: 20), // 간격
+              Text(
+                '반려동물과의 커뮤니케이션', // 부제목
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white, // 텍스트 색상
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,22 @@
+// main.dart
 import 'package:flutter/material.dart';
+import 'features/splash/Splash.dart'; // 실제 홈 화면이 따로 있다면
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.orange, // 주황색 배경
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '댕댕일기', // 제목
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white, // 텍스트 색상
-                ),
-              ),
-              SizedBox(height: 20), // 간격
-              Text(
-                '반려동물과의 커뮤니케이션', // 부제목
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white, // 텍스트 색상
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Splash(),
+      },
     );
   }
 }
